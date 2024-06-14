66432cab3b2eeba96248df96b133b0c327b60caa
Malinga <malinga_samarakoon@outlook.com>
2024-06-14 11:40:35 +0530

    composer.lock added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'malinga/gym-placement-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f5b53371b53b37675b37a1953d830165ce036073',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'malinga/gym-placement-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f5b53371b53b37675b37a1953d830165ce036073',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
